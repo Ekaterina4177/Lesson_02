7c8db0cca5a74a3d0d83c10aa5c5fe14f6fba0b8
Ekaterina <you@example.com>
2022-08-23 21:24:37 +0300

    Задача с массивом

---
+++ Example_002/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int argl, int arg2, int arg3)
+{
+    int result = argl;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 11, 21, 56, 76, 89, 65, 80, 45, 67 };
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
