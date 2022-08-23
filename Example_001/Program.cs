b6d0d09aed3c9a746b75284b475bcebb00034005
Ekaterina <you@example.com>
2022-08-23 21:07:27 +0300

    Второй вариант программы через метод

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,22 +1,24 @@
-﻿int a = 10;
-int b = 11;
-int c = 1;
-int d = 19;
-int e = 17;
-int f = 5;
-int g = 6;
-int k = 4;
-int l = 15;
+﻿int Max(int argl, int arg2, int arg3)
+{
+    int result = argl;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a = 5;
+int b = 4;
+int c = 18;
+int d = 89;
+int e = 45;
+int f = 187;
+int g = 57;
+int k = 49;
+int l = 1800;
 
-int max = a;
+int max1 = Max(a, b, c);
+int max2 = Max(d, e, f);
+int max3 = Max(g, k, l);
 
-if(b > max) max = b;
-if(c > max) max = c;
-if(d > max) max = d;
-if(e > max) max = e;
-if(f > max) max = f;
-if(g > max) max = g;
-if(k > max) max = k;
-if(l > max) max = l;
+int max = Max(max1, max2, max3);
 
 Console.WriteLine(max);
