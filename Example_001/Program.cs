79fdb8b3acbd69487f406991d6eda1cd7f71af03
Ekaterina <you@example.com>
2022-08-23 20:52:57 +0300

    Первая версия решения задачи

---
+++ Example_001/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 10;
+int b = 11;
+int c = 1;
+int d = 19;
+int e = 17;
+int f = 5;
+int g = 6;
+int k = 4;
+int l = 15;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+if(g > max) max = g;
+if(k > max) max = k;
+if(l > max) max = l;
+
+Console.WriteLine(max);
